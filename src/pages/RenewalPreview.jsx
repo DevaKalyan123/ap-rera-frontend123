cbfaebd742e3ef88a298a537ba0e8155a86fcb21
DevaKalyan123 <devakalyaneepi@gmail.com>
2026-05-20 17:13:46 +0530

    renwal

--- src/pages/RenewalPreview.jsx
+++ src/pages/RenewalPreview.jsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from "react";
 import { useParams, useNavigate } from "react-router-dom";
 import { getPreview } from "../api/renewalApi";
 import RenewalStepper from "../components/RenewalStepper";
-import "../styles/renewalPreview.css";
+import "../styles/renewalpreview.css";
 
 function RenewalPreview() {
 
